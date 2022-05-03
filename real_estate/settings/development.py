0776161251cad271b811b75d1858c92e7ca9b889
morteza <mgahangir2006@gmail.com>
2022-05-03 12:04:41 +0430

    'Add user auth-register,login,password reset'

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -1,5 +1,15 @@
 from .base import *
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_EMAIL = "mgahangir2006@gmail.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Real Estate"
+
 DATABASES = {
     "default": {
         "ENGINE": env("POSTGRES_ENGINE"),
