3fe277f4331c7663adf137cdd2430c0f0b6de55b
morteza <mgahangir2006@gmail.com>
2022-05-18 10:45:02 +0430

    'chore: Setup basic home and properties page'

---
+++ client/src/pages/PropertiesPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+
+function PropertiesPage() {
+  return (
+    <>
+        <Container>
+            <Row>
+                <Col className="mg-top">
+                    <h1 className="">Properties will be dispalayed here </h1>
+                </Col>
+            </Row>
+        </Container>
+    </>
+  );
+};
+
+export default PropertiesPage
