0357364500ea6f96489bee00ca65f241239662b9
morteza <mgahangir2006@gmail.com>
2022-05-19 09:03:36 +0430

    'feat: Not Found page'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
@@ -14,15 +15,14 @@ const App=()=> {
         <Header/>
         <main className="py-3">
           <Routes>
-            <Route path="/" element={<HomePage/>}></Route>
-          </Routes>
-          <Routes>
+            <Route path="/" element={<HomePage/>}/>  
             <Route path="/properties" element={<PropertiesPage/>}></Route>
+            <Route path="*" element={<NotFound/>}></Route>
           </Routes>
+          <ToastContainer theme="dark" />
         </main>
         <Footer/>
       </Router>
-      <ToastContainer/>
     </>
   );
 };
